d284941431f458fa65e6f4153343d7acdd3c4ae7
Swati Mandal <swati.m_2000@rediffmail.com>
2024-02-26 02:17:04 +0530

    sorting functionality

--- backend/controllers/Product.controller.js
+++ backend/controllers/Product.controller.js
@@ -1,34 +1,34 @@
 const ProductModel = require("../models/Product.model")
 
-const getProducts = async(req, res) => {
+const getProducts = async (req, res) => {
     try {
-        const { sort, order, filter, q } = req.query;
-
-        let query = {};
-
-        if (sort) {
-            const sortOrder = order === "desc" ? -1 : 1;
-            query = { ...query, [sort]: sortOrder };
-        }
-
-        if (filter) {
-            query = { ...query, name: { $regex: filter, $options: 'i' } }; 
-        }
-
-        if (q) {
-            query = { ...query, name: { $regex: `^${search}`, $options: 'i' } };
-        }
-
-        const products = await ProductModel.find(query);
-        res.status(200).json({ products });
-
+      const { sort, order, filter, q } = req.query;
+  
+      let query = {};
+      let sortQuery = {};
+      let sortOrder;
+  
+      if (sort) {
+        sortOrder = order === "desc" ? -1 : 1;
+        sortQuery[sort] = sortOrder;
+      }
+  
+      if (filter) {
+        query = { ...query, name: { $regex: filter, $options: 'i' } };
+      }
+  
+      if (q) {
+        query = { ...query, name: { $regex: `^${q}`, $options: 'i' } };
+      }
+  
+      const products = await ProductModel.find(query).sort(sortQuery);
+      res.status(200).json({ products });
     } catch (error) {
-
-        console.log(`error: ${error}`)
-        res.status(500).send({error: "internal server error"})
-        
+      console.log(`error: ${error}`);
+      res.status(500).send({ error: "internal server error" });
     }
-}
+  };
+  
 
 const addProduct = async(req, res) => {
     try {
